ec174ee25d13679e365600e5c1fced46ab494b2f
objektwerks <objektwerks@runbox.com>
2025-04-22 17:56:04 -0400

    enhanced main

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -1,4 +1,10 @@
 package objektwerks
 
+import mainargs.{main, arg, ParserForMethods}
+
 object App:
   val store = Store()
+
+  def main(args: Array[String]): Unit =
+    if args.isEmpty then commands()
+    else ParserForMethods(this).runOrExit(args)
