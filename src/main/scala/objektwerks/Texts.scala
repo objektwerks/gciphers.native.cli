41a8bb97f34b3bff45ca9981a43e3d96d5a5394c
objektwerks <objektwerks@runbox.com>
2025-04-22 17:44:30 -0400

    refactored texts

--- src/main/scala/objektwerks/Texts.scala
+++ src/main/scala/objektwerks/Texts.scala
@@ -3,12 +3,8 @@ package objektwerks
 import upickle.default.ReadWriter as JsonSupport
 
 import scala.collection.mutable
-import scalafx.beans.property.ObjectProperty
 
 object Texts:
   given Ordering[Texts] = Ordering.by[Texts, Int](t => t.number)
 
-final case class Texts(number: Int, values: mutable.Set[String]) derives CanEqual, JsonSupport:
-  val numberProperty = ObjectProperty(number)
-  val fileProperty = ObjectProperty(s"$number.json")
-  def valuesToString(): ObjectProperty[String] = ObjectProperty( values.mkString(", ") )
+final case class Texts(number: Int, values: mutable.Set[String]) derives CanEqual, JsonSupport
