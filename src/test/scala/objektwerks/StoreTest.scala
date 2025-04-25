562eea5d080320a91f78527caf08c906614cb83a
objektwerks <objektwerks@runbox.com>
2025-04-25 05:53:43 -0400

    edited store test

--- src/test/scala/objektwerks/StoreTest.scala
+++ src/test/scala/objektwerks/StoreTest.scala
@@ -3,12 +3,10 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-import scala.collection.mutable
-
 class StoreTest extends AnyFunSuite with Matchers:
   test("store"):
     val store = Store()
-    val texts = Texts(1, mutable.Set("a"))
+    val texts = Texts(1, Set("a"))
     store.writeTexts(texts)
     store.readTexts(texts.fileProperty) shouldBe texts
     store.listTexts().length should be >= 1
